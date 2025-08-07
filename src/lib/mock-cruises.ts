9a17ccb5bf1e1a18302775cfd901aa7a0a544536
Carlo Martin <sinestesiacm@gmail.com>
2025-08-07 08:50:57 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <RecommendedCruise

--- src/lib/mock-cruises.ts
+++ src/lib/mock-cruises.ts
@@ -7,7 +7,7 @@ export const recommendedCruises: CruisePackage[] = [
     name: 'Caribbean Dream',
     ship: 'Explorer of the Seas',
     duration: '7 Nights',
-    image: 'https://images.unsplash.com/photo-1574401569788-f1f13a453697?w=500',
+    image: 'https://images.unsplash.com/photo-1662154477069-c8c0a5361a4c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwyfHxjYXJpYmJlYW4lMjBiZWFjaHxlbnwwfHx8fDE3NTQ1NTY2MTF8MA&ixlib=rb-4.1.0&q=80&w=1080',
     hint: 'caribbean beach',
     videoUrl: 'https://videos.pexels.com/video-files/3089163/3089163-hd.mp4',
     carouselImages: [
@@ -34,7 +34,7 @@ export const recommendedCruises: CruisePackage[] = [
     name: 'Alaskan Adventure',
     ship: 'National Geographic Quest',
     duration: '8 Nights',
-    image: 'https://images.unsplash.com/photo-1605333146449-b5cd15f75c2d?w=500',
+    image: 'https://images.unsplash.com/photo-1527022206739-2c7edca55829?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMHx8YWxhc2thJTIwZ2xhY2llcnxlbnwwfHx8fDE3NTQ1NTY2MTF8MA&ixlib=rb-4.1.0&q=80&w=1080',
     hint: 'alaska glacier',
     videoUrl: 'https://videos.pexels.com/video-files/3209828/3209828-hd.mp4',
     carouselImages: [
