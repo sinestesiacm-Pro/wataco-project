c21b19fa592d311cb97265ad1b6709727d67512c
Carlo Martin <sinestesiacm@gmail.com>
2025-09-07 20:37:16 +0000

    quiero que los resultados incluyan mas de una naviera si existe para esa

--- src/lib/mock-cruises.ts
+++ src/lib/mock-cruises.ts
@@ -59,4 +59,58 @@ export const recommendedCruises: CruisePackage[] = [
       { day: 8, port: 'Seward, Alaska', arrival: '08:00', departure: 'Disembark', countryCode: 'us', image: 'https://images.unsplash.com/photo-1548849839-55448b11c3e1' },
     ]
   },
+  {
+    id: 'caribbean-celebration-02',
+    name: 'Southern Caribbean Celebration',
+    ship: 'Celebrity Apex',
+    duration: '7 Nights',
+    region: 'CARIBBEAN',
+    image: 'https://images.unsplash.com/photo-1589463901957-c73eb43a2328?w=500',
+    hint: 'modern cruise ship',
+    videoUrl: 'https://videos.pexels.com/video-files/857251/857251-hd.mp4',
+    carouselImages: [
+      'https://images.pexels.com/photos/1831236/pexels-photo-1831236.jpeg',
+      'https://images.pexels.com/photos/1591373/pexels-photo-1591373.jpeg',
+      'https://images.pexels.com/photos/1638280/pexels-photo-1638280.jpeg'
+    ],
+    rating: 5,
+    reviews: 940,
+    price: '1120',
+    itinerary: [
+      { day: 1, port: 'San Juan, Puerto Rico', arrival: 'Embark', departure: '20:00', countryCode: 'pr', image: 'https://images.unsplash.com/photo-1588728876250-863a49339a13' },
+      { day: 2, port: 'At Sea', arrival: 'High Seas', departure: '', isAtSea: true },
+      { day: 3, port: 'Bridgetown, Barbados', arrival: '08:00', departure: '18:00', countryCode: 'bb', image: 'https://images.unsplash.com/photo-1584880965022-297a7913e2f0' },
+      { day: 4, port: 'Castries, St. Lucia', arrival: '08:00', departure: '17:00', countryCode: 'lc', image: 'https://images.unsplash.com/photo-1596701976263-415f33362145' },
+      { day: 5, port: "St. John's, Antigua", arrival: '08:00', departure: '17:00', countryCode: 'ag', image: 'https://images.unsplash.com/photo-1579453701853-99757c21c37b' },
+      { day: 6, port: 'Basseterre, St. Kitts', arrival: '07:00', departure: '16:00', countryCode: 'kn', image: 'https://images.unsplash.com/photo-1597811902263-54898b50f75e' },
+      { day: 7, port: 'San Juan, Puerto Rico', arrival: '06:00', departure: 'Disembark', countryCode: 'pr', image: 'https://images.unsplash.com/photo-1588728876250-863a49339a13' },
+    ],
+  },
+  {
+    id: 'mediterranean-majesty-01',
+    name: 'Mediterranean Majesty',
+    ship: 'MSC Grandiosa',
+    duration: '7 Nights',
+    region: 'EUROPE',
+    image: 'https://images.unsplash.com/photo-1516496636080-14fb876e029d?w=500',
+    hint: 'cruise ship europe',
+    videoUrl: 'https://videos.pexels.com/video-files/855389/855389-hd.mp4',
+    carouselImages: [
+      'https://images.pexels.com/photos/1365425/pexels-photo-1365425.jpeg',
+      'https://images.pexels.com/photos/2087391/pexels-photo-2087391.jpeg',
+      'https://images.pexels.com/photos/2224850/pexels-photo-2224850.jpeg'
+    ],
+    rating: 4,
+    reviews: 1320,
+    price: '890',
+    itinerary: [
+      { day: 1, port: 'Barcelona, Spain', arrival: 'Embark', departure: '18:00', countryCode: 'es', image: 'https://images.unsplash.com/photo-1523531294919-467a62a94eda' },
+      { day: 2, port: 'Marseille, France', arrival: '08:00', departure: '17:00', countryCode: 'fr', image: 'https://images.unsplash.com/photo-1563503200-a3593368153c' },
+      { day: 3, port: 'Genoa, Italy', arrival: '08:00', departure: '18:00', countryCode: 'it', image: 'https://images.unsplash.com/photo-1588523773934-03a89e02316e' },
+      { day: 4, port: 'Naples, Italy', arrival: '13:00', departure: '20:00', countryCode: 'it', image: 'https://images.unsplash.com/photo-1589133292419-14a4231b4716' },
+      { day: 5, port: 'Messina, Sicily', arrival: '08:00', departure: '18:00', countryCode: 'it', image: 'https://images.unsplash.com/photo-1594959146524-73d8a6d63428' },
+      { day: 6, port: 'Valletta, Malta', arrival: '08:00', departure: '17:00', countryCode: 'mt', image: 'https://images.unsplash.com/photo-1590796338575-8828f731112d' },
+      { day: 7, port: 'At Sea', arrival: 'High Seas', departure: '', isAtSea: true },
+    ]
+  }
 ];
