cd687850a4257a87785be41b7f8a76d87425508a
Carlo Martin <sinestesiacm@gmail.com>
2025-11-28 09:57:11 +0000

    7b693caa6d94519ce17374929121537f
    8ecd58d026
    
    revisando bed, quiere deci

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,26 +4,26 @@ import { getAuth, Auth } from "firebase/auth";
 import { getFirestore, doc } from "firebase/firestore";
 
 // These are public keys and are safe to be exposed in the client-side code
-export const AMADEUS_API_KEY = "8KbSUTGXfLXXnZhk3dvVJcAyhdL6uGKG";
-export const AMADEUS_API_SECRET = "uLzV2uC2xTA9SGar";
+export const AMADEUS_API_KEY = process.env.NEXT_PUBLIC_AMADEUS_API_KEY;
+export const AMADEUS_API_SECRET = process.env.NEXT_PUBLIC_AMADEUS_API_SECRET;
 
 // Hotelbeds Hotels API Credentials
-export const HOTELBEDS_API_KEY = "7b693caa6d94519ce17374929121537f";
-export const HOTELBEDS_SECRET = "8ecd58d026";
+export const HOTELBEDS_API_KEY = process.env.NEXT_PUBLIC_HOTELBEDS_API_KEY;
+export const HOTELBEDS_SECRET = process.env.NEXT_PUBLIC_HOTELBEDS_SECRET;
 
 // Hotelbeds Activities API Credentials (for future use)
-export const HOTELBEDS_ACTIVITIES_API_KEY = "0ecd9c27847f29ca3db2c0375b53d1e0";
-export const HOTELBEDS_ACTIVITIES_SECRET = "a831c2be29";
+export const HOTELBEDS_ACTIVITIES_API_KEY = process.env.NEXT_PUBLIC_HOTELBEDS_ACTIVITIES_API_KEY;
+export const HOTELBEDS_ACTIVITIES_SECRET = process.env.NEXT_PUBLIC_HOTELBEDS_ACTIVITIES_SECRET;
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNbwoek_BuA76NtTdaS-emwDj92jhbLdo",
-  authDomain: "tripify-umvao.firebaseapp.com",
-  projectId: "tripify-umvao",
-  storageBucket: "tripify-umvao.firebasestorage.app",
-  messagingSenderId: "952475896153",
-  appId: "1:952475896153:web:0ff3bee2a694926621a448",
-  measurementId: ""
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
