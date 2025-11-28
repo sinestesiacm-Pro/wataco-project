f8215d6fd32a7dc9c40831586c804205807d96c7
Carlo Martin <sinestesiacm@gmail.com>
2025-11-28 10:08:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "tripify-umvao",
+  "appId": "1:952475896153:web:0ff3bee2a694926621a448",
+  "apiKey": "AIzaSyDNbwoek_BuA76NtTdaS-emwDj92jhbLdo",
+  "authDomain": "tripify-umvao.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "952475896153"
+};
